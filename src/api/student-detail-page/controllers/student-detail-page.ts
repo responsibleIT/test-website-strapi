91e338f5f23f50d17dd1f77ccff76ba3b1eeb4d5
Xue_Dragon <159780673+XueDragon@users.noreply.github.com>
2026-05-28 13:39:49 +0200

    Student detail page

---
+++ src/api/student-detail-page/controllers/student-detail-page.ts
@@ -0,0 +1,7 @@
+/**
+ * student-detail-page controller
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreController('api::student-detail-page.student-detail-page');
