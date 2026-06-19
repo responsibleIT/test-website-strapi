e4884e9457e723bd1d818f5a25b676e13ce0fc3b
Xue_Dragon <159780673+XueDragon@users.noreply.github.com>
2026-06-19 15:51:08 +0200

    update fields and add plugin

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -107,6 +107,43 @@ export interface AdminApiTokenPermission extends Struct.CollectionTypeSchema {
   };
 }
 
+export interface AdminAuditLog extends Struct.CollectionTypeSchema {
+  collectionName: 'strapi_audit_logs';
+  info: {
+    displayName: 'Audit Log';
+    pluralName: 'audit-logs';
+    singularName: 'audit-log';
+  };
+  options: {
+    draftAndPublish: false;
+    timestamps: false;
+  };
+  pluginOptions: {
+    'content-manager': {
+      visible: false;
+    };
+    'content-type-builder': {
+      visible: false;
+    };
+  };
+  attributes: {
+    action: Schema.Attribute.String & Schema.Attribute.Required;
+    createdAt: Schema.Attribute.DateTime;
+    createdBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
+      Schema.Attribute.Private;
+    date: Schema.Attribute.DateTime & Schema.Attribute.Required;
+    locale: Schema.Attribute.String & Schema.Attribute.Private;
+    localizations: Schema.Attribute.Relation<'oneToMany', 'admin::audit-log'> &
+      Schema.Attribute.Private;
+    payload: Schema.Attribute.JSON;
+    publishedAt: Schema.Attribute.DateTime;
+    updatedAt: Schema.Attribute.DateTime;
+    updatedBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
+      Schema.Attribute.Private;
+    user: Schema.Attribute.Relation<'oneToOne', 'admin::user'>;
+  };
+}
+
 export interface AdminPermission extends Struct.CollectionTypeSchema {
   collectionName: 'admin_permissions';
   info: {
@@ -549,6 +586,13 @@ export interface ApiResearchDetailPageResearchDetailPage
       'api::research-detail-page.research-detail-page'
     > &
       Schema.Attribute.Private;
+    P5: Schema.Attribute.Text &
+      Schema.Attribute.CustomField<
+        'plugin::strapi-code-editor-custom-field.code-editor-text',
+        {
+          language: 'javascript';
+        }
+      >;
     partners: Schema.Attribute.Relation<'manyToMany', 'api::partner.partner'>;
     ProjectBeschrijving: Schema.Attribute.Text;
     ProjectTitel: Schema.Attribute.String &
@@ -636,6 +680,19 @@ export interface ApiStudentDetailPageStudentDetailPage
       'component.onderwijseenheid',
       false
     >;
+    Opleiding: Schema.Attribute.JSON &
+      Schema.Attribute.CustomField<
+        'plugin::multi-select.multi-select',
+        ['HBO-ICT\t', 'CMD\t', 'CO\t', 'CB\t', 'FDND\t', '-']
+      > &
+      Schema.Attribute.DefaultTo<'[-]'>;
+    P5: Schema.Attribute.Text &
+      Schema.Attribute.CustomField<
+        'plugin::strapi-code-editor-custom-field.code-editor-text',
+        {
+          language: 'javascript';
+        }
+      >;
     Partners: Schema.Attribute.Relation<'manyToMany', 'api::partner.partner'>;
     ProjectBeschrijving: Schema.Attribute.String;
     ProjectTitel: Schema.Attribute.String &
@@ -1171,6 +1228,7 @@ declare module '@strapi/strapi' {
     export interface ContentTypeSchemas {
       'admin::api-token': AdminApiToken;
       'admin::api-token-permission': AdminApiTokenPermission;
+      'admin::audit-log': AdminAuditLog;
       'admin::permission': AdminPermission;
       'admin::role': AdminRole;
       'admin::session': AdminSession;
